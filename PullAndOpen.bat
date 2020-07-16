2c4d9c8db60d2401d154bb7401966792c083c1c9
Jukka Salminen <jukka.salminen@gmail.com>
2020-07-16 14:58:51 +0300

    jeps

--- PullAndOpen.bat
+++ PullAndOpen.bat
@@ -1,4 +1,4 @@
 git pull
 pause
 
-"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" jiukkajitsu.html
+"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" %~dp0\jiukkajitsu.html
